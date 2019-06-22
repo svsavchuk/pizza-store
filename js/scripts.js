2343c562a24ae789f2ad113bd523e108efcbaf22
Serhiy Savchuk <svsavchuk@hotmail.com>
2019-06-21 17:13:50 -0700

    Add constructor for pizza and variables for toppings.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,26 @@
 
+
+Function Pizza (topping, size) {
+	this.topping = topping;
+	this.size = size)
+	}
+
+  var sizes = ["small", "medium", "large"]
+
+  var tomatoes = {name: "Tomatoes", price: 0.99};
+  var pepperoni = {name: "Pepperoni", price: 0.99};
+  var artichoke = {name: "Artichoke", price: 0.99};
+  var cheese = {name: "Cheese", price: 0.99};
+  
+
+
+  Pizza.prototype.calculateTotal = function() {
+    return t
+  }
+
+
+
+
 $(document).ready(function(){
   $("#form1").submit(function(event) {
     event.preventDefault();
@@ -11,9 +33,9 @@ $(document).ready(function(){
     var number = parseInt(
     $("#number").val());
 
-    //Display results.
-    $("#result-section").show();
-    $("#result-text").html(name+"<br>"+number);
+    // Display results.
+    // $("#result-section").show();
+    // $("#result-text").html(name+"<br>"+number);
 
   });
 });
